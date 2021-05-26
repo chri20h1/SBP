33a5bb74d3d36bfde2cef8011abd21115f4d2d3f
chri20h1 <58981824+chri20h1@users.noreply.github.com>
2021-05-26 10:08:19 +0200

    asdas

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
     <header>
      <nav>
        <ul>
-         <li><img src="img/tietgenLogo.png" alt="logo" width="75" height="75" href="index.php"></li>
+         <li><img src="img/sbLogo.png" alt="logo" width="75" height="75" href="index.php"></li>
          <li><a class="spilNav" href="index.php">Spil</a></li>
          <li><a class="leaderboardNav" href="leaderboard.php">Leaderboard</a></li>
          <li><a class="omOsNav" href="#">Om os</a></li>
