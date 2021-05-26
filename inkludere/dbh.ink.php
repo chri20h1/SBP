a9bb9743fe9199f438f06860a660fd1a54fcf2ab
chri20h1 <58981824+chri20h1@users.noreply.github.com>
2021-05-26 10:00:41 +0200

    kldklgd

--- inkludere/dbh.ink.php
+++ inkludere/dbh.ink.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "loginsystemtgf";
+$dBName = "SBPDatabase";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
