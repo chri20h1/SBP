65889c58f46a222e6d4f11ac93fb5484134cad3b
chri20h1 <58981824+chri20h1@users.noreply.github.com>
2021-05-26 10:46:00 +0200

    asd

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
      <nav>
        <ul>
          <a href="index.php">
-         <li><img src="img/sbLogo.png" alt="logo" width="100" height="75"><a/></li>
+         <li><img src="img/sbLogo.png" alt="logo" width="103" height="75"><a/></li>
          <li><a class="spilNav" href="index.php">Feed</a></li>
          <li><a class="leaderboardNav" href="leaderboard.php">Aftaler</a></li>
          <li><a class="omOsNav" href="#">Grupper</a></li>
