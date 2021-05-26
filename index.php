cff37bc2a865f9bfe71715bd6e32cf76f0482b2d
chri20h1 <58981824+chri20h1@users.noreply.github.com>
2021-05-26 11:19:25 +0200

    dsd

--- index.php
+++ index.php
@@ -8,17 +8,21 @@
        <section class="section-default">
          <?php
           if (isset($_SESSION['userId'])) {
-            echo '<p class="login-status">Du er logget på!!!!</p>';
-            $sql = "SELECT * FROM brugere";
-            $result = mysqli_query($conn, $sql);
-            $resultCheck = mysqli_num_rows($result);
-
-          if ($resultCheck > 0) {
-            while($row = mysqli_fetch_assoc($result)) {
-            echo $row['emailBrugere'];
-           }
-          }
+            echo '<p class="login-status">Du er logget på!!!!</p>
+            <form class="highscoreSubmit" action="inkludere/highscore.ink.php" method="post">
+             <input type="text" name="highscore" class="input-box" placeholder="Hvad har du på hjertet? <3" id="txthighscore">
+             <button type="submit" class="highscore-submit" name="highscore-submit">Slå op</button>
+            ';
+            // $sql = "SELECT * FROM brugere";
+            // $result = mysqli_query($conn, $sql);
+            // $resultCheck = mysqli_num_rows($result);
+            // if ($resultCheck > 0) {
+            //  while($row = mysqli_fetch_assoc($result)) {
+            //  echo $row['emailBrugere'];
+          // }
+        //  }
          }
+
           else {
             echo '<p class="login-status">Du er ikke logget på!</p>';
           }
